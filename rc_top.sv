ea8bb38938ac826295d464f20551702b066cdb8c
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2023-05-02 13:27:16 -0700

    Remove extra commas

--- rc_top.sv
+++ rc_top.sv
@@ -1,18 +1,18 @@
 `include "rc_model.sv"
 
 module rc_top #(
-    `DECL_REAL(result),
+    `DECL_REAL(result)
 ) (
     input wire logic clk,
     input wire logic rst,
-    `OUTPUT_REAL(result),
+    `OUTPUT_REAL(result)
 );
   `MAKE_CONST_REAL(1.0, in);
   `MAKE_REAL(result, 10.0);
 
   rc_model #(
       `PASS_REAL(v_in, in),
-      `PASS_REAL(v_out, result),
+      `PASS_REAL(v_out, result)
   ) model(.v_in(in), .v_out(result), .clk(clk), .rst(rst));
 
   `MAKE_CONST_REAL(2.0, val);
