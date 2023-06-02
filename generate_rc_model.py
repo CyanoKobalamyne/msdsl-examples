af8635b62e50bacccbca71a035e21327a7c37ad1
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2023-06-01 17:06:09 -0700

    Format Python

--- generate_rc_model.py
+++ generate_rc_model.py
@@ -2,11 +2,11 @@ from msdsl import MixedSignalModel, VerilogGenerator
 from math import exp
 
 r, c, dt = 1e3, 1e-9, 0.1e-6
-m = MixedSignalModel('rc_model')
-x = m.add_analog_input('v_in')
-y = m.add_analog_output('v_out')
-clk = m.add_digital_input('clk')
-rst = m.add_digital_input('rst')
-a = exp(-dt/(r*c))
-m.set_next_cycle(y, a * y + (1-a) * x, clk=clk, rst=rst)
+m = MixedSignalModel("rc_model")
+x = m.add_analog_input("v_in")
+y = m.add_analog_output("v_out")
+clk = m.add_digital_input("clk")
+rst = m.add_digital_input("rst")
+a = exp(-dt / (r * c))
+m.set_next_cycle(y, a * y + (1 - a) * x, clk=clk, rst=rst)
 m.compile_and_print(VerilogGenerator())
